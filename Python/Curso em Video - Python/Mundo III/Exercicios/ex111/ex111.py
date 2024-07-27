c6ed3247740ce6c4ffaccdbeb0d8f07433124375
Deiverson Garcia <deiverson.prog@gmail.com>
2024-07-27 13:28:46 -0300

    correção de caminhos

--- Python/Curso em Video - Python/Mundo III/Exercicios/ex111/ex111.py
+++ Python/Curso em Video - Python/Mundo III/Exercicios/ex111/ex111.py
@@ -2,9 +2,8 @@
 moeda e dado. Transfira todas as funções utilizadas nos desafios 107, 108 e 109
 para o primeiro pacote e mantenha tudo funcionando.'''
 
-#from ex111.utilidadescev import moeda
-#from ex111.utilidadescev import dado
-from moeda import moeda #verificar como importar no VSCode
+from utilidadescev import moeda #verificar como importar no VSCode
+from utilidadescev import dado 
 
 p = float(input('Digite o preço: '))
 moeda.resumo(p,12,20)
--- Python/Curso em Video - Python/Mundo III/Exercicios/ex112/ex112.py
+++ Python/Curso em Video - Python/Mundo III/Exercicios/ex112/ex112.py
@@ -3,9 +3,7 @@ chamado dado. Cria uma função chamada leiaDinheiro() que seja capaz de
 funcionar como a função input(), mas com uma validação de dados para aceitar
 apenas valores que sejam monetários.'''
 
-#from ex112.utilidadescev import moeda
-#from ex112utilidadescev import dado
-from moeda import moeda #verificar como importar no VSCode
-
+from utilidadescev import dado
+from utilidadescev import moeda 
 p = dado.leiaDinheiro('Digite o preço: R$ ')
 moeda.resumo(p,35,22)
